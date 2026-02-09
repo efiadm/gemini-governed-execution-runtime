5753cec31ada7a20b04d5d8305f0ceb98c2c7c44
Nicholas Evans <differentengine550@gmail.com>
2026-02-09 03:18:46 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Layout({ children, currentPageName }) {
+  useEffect(() => {
+    // Activate dark theme
+    document.documentElement.classList.add('dark');
+  }, []);
+
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background text-foreground">
       {children}
     </div>
   );
